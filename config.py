0127cfd3c1d797ebc010e3e9fb00119d508ce641
koyoo--maxwel <koyoomaxwel@.com>
2018-09-18 15:48:26 +0300

    updated db

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'blog'
+    SUBJECT_PREFIX = 'blogmaster'
     SENDER_EMAIL = 'juantechno.marketing@gmail.com'
     
 
